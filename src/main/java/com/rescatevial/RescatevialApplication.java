6db1ef3d3ca72020d97fc5bc6105ca9fb988a60f
Jean8099324 <124542833+Jean8099324@users.noreply.github.com>
2026-09-16 04:24:08 -0600

    Creacion inicial del proyecto Rescate Vial CR

---
+++ src/main/java/com/rescatevial/RescatevialApplication.java
@@ -0,0 +1,13 @@
+package com.rescatevial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescatevialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescatevialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rescatevial/RescatevialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rescatevial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescatevialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
